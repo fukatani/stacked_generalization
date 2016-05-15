088f1fc6597be20c1ab6f5b980c816215bfe9e3f
fukatani <nannyakannya@gmail.com>
2016-05-15 15:21:23 +0900

    add test

--- lib/test/test.py
+++ lib/test/test.py
@@ -1,5 +1,5 @@
 import unittest
-from stacked_generalization.lib.util import TwoStageKFold
+from stacked_generalization.lib.util import TwoStageKFold, numpy_c_concatenate
 import numpy as np
 
 
@@ -23,6 +23,13 @@ class TestUtil(unittest.TestCase):
         np.testing.assert_equal(train_index, [0, 1, 2])
         np.testing.assert_equal(blend_index, [6, 7])
         np.testing.assert_equal(test_index, [3, 4, 5])
+
+    def test_concatenate(self):
+        A = None
+        B = np.array([[1,2],[3,4]])
+        np.testing.assert_equal(numpy_c_concatenate(A, B), B)
+        A = np.array([[0], [1]])
+        np.testing.assert_equal(numpy_c_concatenate(A, B), [[0,1,2], [1,3,4]])
 
 if __name__ == '__main__':
     unittest.main()
