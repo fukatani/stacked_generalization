2223695ba8baca3bb6bf39b254943d6e83a419bc
fukatani <nannyakannya@gmail.com>
2016-05-18 21:30:44 +0900

    example: compare with gradient boosting

--- stacked_generalization/example/cross_validation_for_iris.py
+++ stacked_generalization/example/cross_validation_for_iris.py
@@ -8,6 +8,7 @@ from stacked_generalization.lib.stacking import StackedClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression, RidgeClassifier
 from sklearn.cross_validation import StratifiedKFold
 
@@ -19,14 +20,17 @@ iris.target = iris.target[perm]
 
 # Stage 1 model
 bclf = LogisticRegression(random_state=1)
+
 # Stage 0 models
 clfs = [RandomForestClassifier(n_estimators=40, criterion = 'gini', random_state=1),
         ExtraTreesClassifier(n_estimators=30, criterion = 'gini', random_state=3),
         GradientBoostingClassifier(n_estimators=25, random_state=1),
+        GradientBoostingClassifier(n_estimators=30, random_state=2),
+        #GradientBoostingClassifier(n_estimators=30, random_state=3),
         RidgeClassifier(random_state=1),
         ]
 
-sl = StackedClassifier(bclf,
+sc = StackedClassifier(bclf,
                        clfs,
                        n_folds=3,
                        verbose=0,
@@ -34,8 +38,11 @@ sl = StackedClassifier(bclf,
                        oob_score_flag=True,
                        )
 
+gb = GradientBoostingClassifier(n_estimators=25, random_state=1)
+
 # cross validation
-cv_score = 0
+sc_score = 0
+gb_score = 0
 n_folds = 3
 for train_idx, test_idx in StratifiedKFold(iris.target, n_folds):
     xs_train = iris.data[train_idx]
@@ -43,9 +50,13 @@ for train_idx, test_idx in StratifiedKFold(iris.target, n_folds):
     xs_test = iris.data[test_idx]
     y_test = iris.target[test_idx]
 
-    sl.fit(xs_train, y_train)
-    print('oob_score: {0}'.format(sl.oob_score_))
-    cv_score += sl.score(xs_test, y_test)
-cv_score /= n_folds
+    sc.fit(xs_train, y_train)
+    print('oob_score: {0}'.format(sc.oob_score_))
+    sc_score += sc.score(xs_test, y_test)
+    gb.fit(xs_train, y_train)
+    gb_score += gb.score(xs_test, y_test)
 
-print('cv_score: {0}'.format(cv_score))
+sc_score /= n_folds
+print('Stacked Classfier score: {0}'.format(sc_score))
+gb_score /= n_folds
+print('Gradient Boosting Classfier score: {0}'.format(gb_score))
