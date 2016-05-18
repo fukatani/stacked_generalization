94652087c9754291cd084b268b2633d49558b1d6
fukatani <nannyakannya@gmail.com>
2016-05-18 21:47:49 +0900

    compare with gradient boosting regressor

--- stacked_generalization/example/simple_regression.py
+++ stacked_generalization/example/simple_regression.py
@@ -14,16 +14,31 @@ from sklearn.linear_model import LinearRegression
 
 
 boston = datasets.load_boston()
+
 X = preprocessing.StandardScaler().fit_transform(boston.data)
+Y = boston.target
+
+X_train = X[:200]
+Y_train = Y[:200]
+X_test = X[200:]
+Y_test = Y[200:]
+
 breg = LinearRegression()
 regs = [RandomForestRegressor(n_estimators=50, random_state=1),
         GradientBoostingRegressor(n_estimators=25, random_state=1),
-        ExtraTreesRegressor(),
-        Ridge(random_state=1)]
+        GradientBoostingRegressor(n_estimators=30, random_state=2),
+        ExtraTreesRegressor()
+        ]
+
 sr = StackedRegressor(breg,
                       regs,
                       n_folds=3,
                       verbose=0)
-sr.fit(X, boston.target)
-score = metrics.mean_squared_error(sr.predict(X), boston.target)
-print ("MSE: %f" % score)
+sr.fit(X_train, Y_train)
+score = metrics.mean_squared_error(sr.predict(X_test), Y_test)
+print ("MSE of stacked regressor: %f" % score)
+
+gb = GradientBoostingRegressor(n_estimators=25, random_state=1)
+gb.fit(X_train, Y_train)
+score = metrics.mean_squared_error(gb.predict(X_test), Y_test)
+print ("MSE of gradient boosting regressor: %f" % score)
