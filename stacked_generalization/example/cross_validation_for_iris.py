5f4f0dbf482cf70be14d3ad5a75fee2fca04ef38
fukatani <nannyakannya@gmail.com>
2016-11-08 21:24:29 +0900

    travis

--- stacked_generalization/example/cross_validation_for_iris.py
+++ stacked_generalization/example/cross_validation_for_iris.py
@@ -1,6 +1,5 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from sklearn import datasets
 from sklearn.utils.validation import check_random_state
--- stacked_generalization/lib/test/test.py
+++ stacked_generalization/lib/test/test.py
@@ -1,7 +1,6 @@
 import unittest
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 
 
 from sklearn import datasets
