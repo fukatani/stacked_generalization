fb5ff52c65dd7fd5c4d854e0067f469ff7af3c1b
Unknown <nannyakannya@gmail.com>
2018-05-26 12:19:22 +0900

    Fix test

--- stacked_generalization/lib/test/test.py
+++ stacked_generalization/lib/test/test.py
@@ -1,7 +1,5 @@
 import unittest
 import os
-import sys
-
 
 from sklearn import datasets
 from sklearn.utils.validation import check_random_state
@@ -91,7 +89,7 @@ class TestStackedClassfier(unittest.TestCase):
         sl.score(self.iris.data, self.iris.target)
         self.assertGreater(score, 0.8, "Failed with score = {0}".format(score))
 
-        self.assertTrue(os.path.isfile('ExtraTreesClassifier_r0_3__m5_2__m4_1__m6_0p0__m1_auto__m0_N__m3_1e-07__m2_N__n0_30__b0_0__c1_gini__c0_N_190.csv'))
+        self.assertTrue(glob.glob('ExtraTreesClassifier_*.csv'))
         for csv_file in glob.glob("*.csv"):
             os.remove(csv_file)
         for csv_file in glob.glob("*.pkl"):
@@ -138,7 +136,14 @@ class TestStackedClassfier(unittest.TestCase):
         model.fit(self.iris.data, self.iris.target)
         indexes = np.fromfunction(lambda x: x, (self.iris.data.shape[0], ), dtype=np.int32)
         saving_predict_proba(model, self.iris.data, indexes)
-        os.remove('RandomForestClassifier_r0_N__m5_2__m4_1__m6_0p0__m1_auto__m0_N__m3_1e-07__m2_N__n0_10__b0_1__c1_gini__c0_N_190.csv')
+        any_file_removed = False
+        for filename in os.listdir():
+            if filename.startswith('RandomForestClassifier'):
+                os.remove(filename)
+                any_file_removed = True
+        self.assertTrue(any_file_removed)
+
+
     def test_fwls_classfier(self):
         feature_func = lambda x: np.ones(x.shape)
         bclf = LogisticRegression(random_state=1)
